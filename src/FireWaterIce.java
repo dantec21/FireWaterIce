296ae347864c2aee3860818e0cfa2612258c8851
Dante <dante.collier.2022@student.lhschools.org>
2019-05-09 13:14:49 -0400

    Intro code written

---
+++ src/FireWaterIce.java
@@ -0,0 +1,17 @@
+public class FireWaterIce {
+    public static void main(String []args){
+        System.out.println("Fire, Water, Ice");
+        System.out.println("Water beats fire");
+        System.out.println("Fire beats ice");
+        System.out.println("Ice beats water");
+        while (1==1){
+            System.out.println("Pick between the three elements and I will tell you whether or not you beat me.");
+            System.exit(0);
+        }
+
+    }
+
+
+
+
+}
