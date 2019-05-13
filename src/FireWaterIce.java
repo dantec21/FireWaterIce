97051187e3eb6a3d0de665f4a1da4808114c235d
Dante <dante.collier.2022@student.lhschools.org>
2019-05-13 14:19:12 -0400

    Started first switch statement

--- src/FireWaterIce.java
+++ src/FireWaterIce.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class FireWaterIce {
     public static void main(String []args){
+        //int [] myNumbers = new int[5];
+       // for(int i=0;i<myNumbers.length );
+        Scanner keyboard = new Scanner(System.in);
+        while (1==1){
         System.out.println("Fire, Water, Ice");
         System.out.println("Water beats fire");
         System.out.println("Fire beats ice");
         System.out.println("Ice beats water");
-        while (1==1){
-            System.out.println("Pick between the three elements and I will tell you whether or not you beat me.");
-            System.exit(0);
+        System.out.println("Pick between the three elements and I will tell you whether or not you beat me.");
+        String decision = keyboard.nextLine();
+        game(decision);
         }
 
     }
+    public static void game(String decision){
+        int pickedNumber = (int) (Math.random()* 3);
+        switch (pickedNumber){
+            case 0:
+
+        }
+
+    }
+
 
 
 
