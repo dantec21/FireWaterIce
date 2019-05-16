fae83ff7a4b3962f40f10c1159b20e64dd97a485
Dante <dante.collier.2022@student.lhschools.org>
2019-05-16 13:11:56 -0400

    Game method complete

--- src/FireWaterIce.java
+++ src/FireWaterIce.java
@@ -10,7 +10,8 @@ public class FireWaterIce {
         System.out.println("Fire beats ice");
         System.out.println("Ice beats water");
         while (1==1){
-        System.out.println(game());
+            System.out.println("1 is fire, 2 is water, 3 is ice.");
+            game();
         }
 
     }
@@ -20,22 +21,42 @@ public class FireWaterIce {
         int decision = keyboard.nextInt();
         // 1 is fire, 2 is water, 3 is ice.
         int pickedNumber = (int) (Math.random()* 3);
-        // o is fire, 1 is water, 2 is ice
-        if (pickedNumber == 0){
-            return "I have picked fire, you ";
-        }
+        // 0 is fire, 1 is water, 2 is ice
         String winner;
-        if (decision==1&&pickedNumber==)
-
-
-
-
-
+        if (decision==1){
+            if (pickedNumber==0){
+                System.out.println("We tied");
+            }
+            if (pickedNumber==1){
+                System.out.println("You lost");
+            }
+            if (pickedNumber==2){
+                System.out.println("You won");
+            }
+        }
+        if (decision==2){
+            if (pickedNumber==0){
+                System.out.println("You won");
+            }
+            if (pickedNumber==1){
+                System.out.println("We tied");
+            }
+            if (pickedNumber==2) {
+                System.out.println("You lost");
+            }
+        }
+        if (decision==3){
+            if (pickedNumber==0){
+                System.out.println("You lost");
+            }
+            if (pickedNumber==1){
+                System.out.println("You won");
+            }
+            if (pickedNumber==2) {
+                System.out.println("We tied");
+            }
+        }
         return "error";
-
-
-
-
     }
 
 
