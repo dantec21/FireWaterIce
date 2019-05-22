916ad0ae53bb072e41c73f724da3aeb260b9a624
Dante <dante.collier.2022@student.lhschools.org>
2019-05-22 14:13:25 -0400

    Finished Project

--- src/FireWaterIce.java
+++ src/FireWaterIce.java
@@ -1,69 +1,85 @@
 import java.util.Scanner;
 
 public class FireWaterIce {
-    public static void main(String []args){
-
-
-        //System.out.println("Fire, Water, Ice");
-        //System.out.println("Water beats fire");
-        //System.out.println("Fire beats ice");
-        //System.out.println("Ice beats water");
-        //while (1==1){
-         //   System.out.println("1 is fire, 2 is water, 3 is ice.");
-          //  game();
-        //}
+    public static void main(String args[]) {
         int [] myNumbers = new int[5];
         for(int i=0;i<myNumbers.length;i++) {
-            System.out.println(myNumbers[i]+1);
+        }
+        System.out.println("Fire, Water, Ice");
+        System.out.println("Water beats fire");
+        System.out.println("Fire beats ice");
+        System.out.println("Ice beats water");
+
+
+        System.out.println("1 is fire, 2 is water, 3 is ice.");
+        game();
+
+
     }
-    public static String game(){
+
+    public static int game() {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Pick between the three elements and I will tell you whether or not you beat me.");
-        int decision = keyboard.nextInt();
-        // 1 is fire, 2 is water, 3 is ice.
-        int pickedNumber = (int) (Math.random()* 3);
-        // 0 is fire, 1 is water, 2 is ice
-        String winner;
-        if (decision==1){
-            if (pickedNumber==0){
-                System.out.println("We tied");
+        int wins = 0;
+        int losses = 0;
+
+        while (1 == 1) {
+            System.out.println("Pick between the three elements and I will tell you whether or not you beat me.");
+            int decision = keyboard.nextInt();
+            // 1 is fire, 2 is water, 3 is ice.
+            int pickedNumber = (int) (Math.random() * 3);
+            // 0 is fire, 1 is water, 2 is ice
+
+            if (decision == 1) {
+                if (pickedNumber == 0) {
+                    System.out.println("We tied");
+                }
+                if (pickedNumber == 1) {
+                    System.out.println("You lost");
+                    losses++;
+                }
+                if (pickedNumber == 2) {
+                    System.out.println("You won");
+                    wins++;
+                }
             }
-            if (pickedNumber==1){
-                System.out.println("You lost");
+            if (decision == 2) {
+                if (pickedNumber == 0) {
+                    System.out.println("You won");
+                    wins++;
+                }
+                if (pickedNumber == 1) {
+                    System.out.println("We tied");
+                }
+                if (pickedNumber == 2) {
+                    System.out.println("You lost");
+                    losses++;
+                }
             }
-            if (pickedNumber==2){
-                System.out.println("You won");
+            if (decision == 3) {
+                if (pickedNumber == 0) {
+                    System.out.println("You lost");
+                    losses++;
+                }
+                if (pickedNumber == 1) {
+                    System.out.println("You won");
+                    wins++;
+
+                }
+                if (pickedNumber == 2) {
+                    System.out.println("We tied");
+                }
             }
-        }
-        if (decision==2){
-            if (pickedNumber==0){
-                System.out.println("You won");
-            }
-            if (pickedNumber==1){
-                System.out.println("We tied");
-            }
-            if (pickedNumber==2) {
-                System.out.println("You lost");
-            }
-        }
-        if (decision==3){
-            if (pickedNumber==0){
-                System.out.println("You lost");
-            }
-            if (pickedNumber==1){
-                System.out.println("You won");
-            }
-            if (pickedNumber==2) {
-                System.out.println("We tied");
-            }
-        }
-        return "error";
+            System.out.println("You have " + wins + " wins and " + losses + " losses.");
 
         }
     }
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
